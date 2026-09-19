41cd349caaff47dd00392b3f4a43ab939d5ec498
juniorpotenza <juniorpotenza@gmail.com>
2026-09-19 16:55:56 -0300

    Etapa 8: versionar endurecimento do scanner e legado

--- wooadventure-integration/wooadventure-integration.php
+++ wooadventure-integration/wooadventure-integration.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: WooAdventure Integration
  * Description: Integração completa para Ecoturismo (Checkout, API Roca, Gestão de Participantes, Agenda, Assinaturas e Check-in).
- * Version: 2.10.0
+ * Version: 2.10.1
  * Author: Seu Nome
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'WCAI_VERSION', '2.10.0' );
+define( 'WCAI_VERSION', '2.10.1' );
 
 if ( ! function_exists( 'wcai_safe_require' ) ) {
     function wcai_safe_require( $filename ) {
