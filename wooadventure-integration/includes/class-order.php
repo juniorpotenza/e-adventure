d5efdf3b437f43d73ac21ca2a4aec9f5448ba690
juniorpotenza <juniorpotenza@gmail.com>
2026-09-21 17:19:04 -0300

    refactor: derive legacy tour date from departure

--- wooadventure-integration/includes/class-order.php
+++ wooadventure-integration/includes/class-order.php
@@ -2,6 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class WCAI_Order {
+
     public function __construct() {
         add_action( 'woocommerce_checkout_create_order', array( $this, 'add_tour_date_meta' ), 20, 2 );
         add_filter( 'woocommerce_order_number', array( $this, 'custom_order_number' ), 10, 2 );
@@ -9,21 +10,44 @@ class WCAI_Order {
 
     public function add_tour_date_meta( $order, $data ) {
         foreach ( $order->get_items() as $item ) {
-            $tour_date = $item->get_meta( 'tour_date' );
+            $departure_id = WCAI_Data_Resolver::get_departure_id_for_order_item( $item );
+
+            if ( $departure_id ) {
+                $start = WCAI_Data_Resolver::get_departure_start( $departure_id );
+
+                if ( $start ) {
+                    // Mantém o campo legado para aplicações existentes.
+                    $order->update_meta_data( 'tour_date', $start );
+                    return;
+                }
+            }
+
+            $tour_date = $item->get_meta( 'tour_date', true );
             if ( $tour_date ) {
                 $order->update_meta_data( 'tour_date', $tour_date );
-                break; 
+                return;
             }
         }
     }
 
     public function custom_order_number( $order_number, $order ) {
-        $tour_date = $order->get_meta( 'tour_date' );
-        if ( $tour_date ) {
-            $timestamp = strtotime( $tour_date );
-            $formatted_date = date( 'd/m/Y H:i', $timestamp );
-            return $order_number . ' - ' . $formatted_date;
+        $datetime = '';
+
+        foreach ( $order->get_items() as $item ) {
+            $datetime = WCAI_Data_Resolver::get_event_datetime_for_order_item( $order, $item );
+            if ( $datetime ) {
+                break;
+            }
         }
+
+        if ( $datetime ) {
+            $timestamp = WCAI_Data_Resolver::parse_timestamp( $datetime );
+
+            if ( $timestamp ) {
+                return $order_number . ' - ' . wp_date( 'd/m/Y H:i', $timestamp );
+            }
+        }
+
         return $order_number;
     }
 }
